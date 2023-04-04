54cdcf752287bef92836bae63318ebf6202b8efc
sreeharsha-alluri <53439217+sreeharsha-alluri@users.noreply.github.com>
2023-04-04 10:33:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+  ami           = "ami-0aa2b7722dc1b5612"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
