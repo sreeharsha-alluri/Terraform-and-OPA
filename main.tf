33fc4910e36a86a6d5dc8e3b652a97fa2020bdde
sreeharsha-alluri <53439217+sreeharsha-alluri@users.noreply.github.com>
2023-04-04 14:52:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami           = "ami-0aa2b7722dc1b5612"
   tags = {
-    Name = "HelloWorld"
+    Name = "Terraform_Instance"
     Contact = "sreeharsha@gmail.com"
   }
 }
