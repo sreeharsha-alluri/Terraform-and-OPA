5affa3cfc5da22e89e609dd1b3e2e4e4fbe12fb1
sreeharsha-alluri <53439217+sreeharsha-alluri@users.noreply.github.com>
2023-04-04 13:10:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0aa2b7722dc1b5612"
   tags = {
     Name = "HelloWorld"
-    Contanct ="sreeharsa@gmail.com"
+    Contact ="sreeharsha@gmail.com"
   }
 }
