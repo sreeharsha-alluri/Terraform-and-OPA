3b4a4ad856307bfbf4bf10ba4e15aba05fca658b
sreeharsha-alluri <53439217+sreeharsha-alluri@users.noreply.github.com>
2023-04-07 15:47:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  regi = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami           = "ami-0aa2b7722dc1b5612"
-  tags = {
+  tag = {
     Name = "Terraform_Instance_through_OPA"
+    
   }
 }
